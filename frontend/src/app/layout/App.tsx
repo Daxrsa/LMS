6a215fdc3c5015da8fd9e19d6779097f469225f5
Era Graiçevci <93553954+Eragraicevci@users.noreply.github.com>
2023-02-10 11:28:26 +0100

    signUp

--- frontend/src/app/layout/App.tsx
+++ frontend/src/app/layout/App.tsx
@@ -5,10 +5,11 @@ import { Exam } from "../models/exam";
 import NavBar from "./NavBar";
 import { Container } from "semantic-ui-react";
 import ExamDashboard from "../../features/exams/dashboard/ExamDashboard";
+import Register from "./Register";
 
 function App() {
   const [exams, setExams] = useState<Exam[]>([]);
-
+  const [currentForm, setCurrentForm]=useState('login');
   useEffect(() => {
     axios.get<Exam[]>("http://localhost:5113/api/Exam").then((response) => {
       console.log(response);
@@ -16,10 +17,19 @@ function App() {
     });
   }, []);
 
+//it knows which name it should set
+ /* const toggleForm =(formName: any)=> { 
+    setCurrentForm(formName);
+  }*/
+
   return (
     <>
       <NavBar />
         <Container style={{ marginTop: "7em" }}>
+          {
+           // currentForm=="login" ?<Login onFormSwitch ={toggleForm}/> : <Register  onFormSwitch ={toggleForm}/>
+          }
+          <Register/>
           <ExamDashboard exams={exams} />
         </Container>
     </>
